083d12c76bc0a12d7ef7ea32e862f0924ef8ed2e
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2022-11-10 18:39:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Password from './component/Password';
+
 import Ps from './component/Ps';
 
 function App() {
   return (
     <div className="Apps">
-     {/* <Password/> */}
+   
      <Ps/>
     </div>
   );
